ccff72f66718399cd6b2eb68f68e138602e63381
AnkithReddy02 <ankithreddy2001@gmail.com>
2019-10-07 21:50:04 +0530

    Q1 program

---
+++ Q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float n1, n2, n3;
+
+    printf("Enter three different numbers: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    if( n1>=n2 && n1>=n3 )
+        printf("%f is the largest number.", n1);
+
+    if( n2>=n1 && n2>=n3 )
+        printf("%f is the largest number.", n2);
+
+    if( n3>=n1 && n3>=n2 )
+        printf("%f is the largest number.", n3);
+
+    return 0;
+}
+
