6a945b3f1d5b3d652a944167dfaa409639e90e41
Mandara Sharannya <mandarasharannya990116@gmail.com>
2019-10-11 15:23:55 +0530

    Update and rename omii99.Q2.py to omii99.Q2.c

--- Question_2/omii99.Q2.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number"))
-
-if num == 0:
-    print("Enter a valid number")
-elif(num % 2) == 0:
-    print(f"{num} is an even number")
-else:
-    print(f"{num} is an odd number")
