459c5bd6eac17edd7c69efa037c3d3b33f9536c5
Rohankumar11 <rkkvbksc3@gmail.com>
2019-10-12 18:36:18 +0530

    to check prime number

--- rohankumar11.Q3.c
+++ rohankumar11.Q3.c
@@ -4,10 +4,10 @@ int main()
 
 int n,i,p=0;
     printf("Enter a positive integer: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     for(i=2;i<=n/2;++i)
     {
-        // condition for nonprime number
+
         if(n%i==0)
         {
             p=1;
@@ -20,7 +20,7 @@ int n,i,p=0;
     }
     else
     {
-        if (p==0)
+        if(p==0)
           printf("%d is a prime number.", n);
         else
           printf("%d is not a prime number.", n);
@@ -28,3 +28,4 @@ int n,i,p=0;
 
     return 0;
 }
+
