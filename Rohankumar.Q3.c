a63836c8be8c9a07689b089be38896d257a19bd6
Rohankumar11 <rkkvbksc3@gmail.com>
2019-10-12 18:10:12 +0530

    to check a number is prime is not

--- Rohankumar.Q3.c
+++ Rohankumar.Q3.c
@@ -2,25 +2,25 @@
 int main()
 {
 
-int n, i, flag = 0;
+int n,i,p=0;
     printf("Enter a positive integer: ");
-    scanf("%d", &n);
-    for(i = 2; i <= n/2; ++i)
+    scanf("%d",&n);
+    for(i=2;i<=n/2;++i)
     {
         // condition for nonprime number
-        if(n%i == 0)
+        if(n%i==0)
         {
-            flag = 1;
+            p=1;
             break;
         }
     }
-    if (n == 1)
+    if(n==1)
     {
-      printf("1 is neither a prime nor a composite number.");
+      printf("1 is not a prime number");
     }
     else
     {
-        if (flag == 0)
+        if (p==0)
           printf("%d is a prime number.", n);
         else
           printf("%d is not a prime number.", n);
