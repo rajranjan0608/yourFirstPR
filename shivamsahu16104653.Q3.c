68bb58917ab6e4c4995d00add4205c9cda957859
shivamsahu16104653 <shivshivamsahu@gmail.com>
2019-10-16 00:36:22 +0530

    check  numbers is prime or not prime

---
+++ shivamsahu16104653.Q3.c
@@ -0,0 +1,17 @@
+   #include<stdio.h>
+  int main()
+  {int n,i,flage=0;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+
+        for(i=2;i<n;i++)
+       if(n%i!=0)
+        flage=1;
+        else
+         {printf("Number is not Prime ");flage=0;
+        break;}
+        if(flage==1||n==2)
+         printf("Number is Prime ");
+        if(n==1)
+         printf("Number is not prime ");
+  return 0;}
