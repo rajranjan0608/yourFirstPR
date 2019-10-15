762662ecdb0312eb9aa2fbb1a1f653ebd03cf695
rahul-soni123 <56204530+rahul-soni123@users.noreply.github.com>
2019-10-16 02:24:57 +0530

    Updated Question 7.

--- rahul-soni123.Q7.c
+++ rahul-soni123.Q7.c
@@ -1,23 +1,37 @@
 #include<stdio.h>
 int main()
 {
-int a,b,i,min,flag;
+    int a,b,i,min,flag;
 	printf("Enter the two numbers:");
 	scanf("%d %d",&a,&b);
-	if(a<b)
-	{
-		min=a;
-	}
-	else 
-	{
-		min=b;
-	}
-	for(i=1;i<=min;i++)
-	{
-		if(((a%i)==0)&&((b%i)==0))
-	        flag=i;
-	}
-	printf("The greatest common divisor of the two numbers is:%d",flag);
+	if((a==0)&&(b==0))
+		printf("The greatest common divisor is not defined");
+        else if(a==0)
+		flag=b;
+	
+	else if(b==0)
+		flag=a;
+        else{
+         	if(a<=b)
+                  {
+	           min=a;
+	          }
+
+	        else 
+	          {
+		   min=b;
+	          }
+
+	        for(i=1;i<=min;i++)
+	           {
+		    if(((a%i)==0)&&((b%i)==0))
+	               flag=i;
+	           }
+
+             }
+      if((a!=0)||(b!=0))	
+     printf("The greatest common divisor of the two numbers is:%d",flag);
+    
 return 0;
 }
 
