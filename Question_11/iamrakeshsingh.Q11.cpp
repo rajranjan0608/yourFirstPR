adb40762e5d262a4edc56d914f4e939ceae73ffb
iamrakeshsingh <55805402+iamrakeshsingh@users.noreply.github.com>
2020-10-06 21:43:08 +0530

    Create iamrakeshsingh.Q11.cpp

---
+++ Question_11/iamrakeshsingh.Q11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b;
+  cout << "Enter two numbers: ";  cin >> a >> b;  cout << "\n";
+  cout << a-(-b) << "\n";
+  
+  return 0;
+}
