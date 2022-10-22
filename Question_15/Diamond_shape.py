e54395d0cfab5384680a8df25ff3d39453268e54
Sanjeev Kumar <premchandrasingh786@gmail.com>
2022-10-22 08:19:15 +0530

    Update Diamond_shape.py

--- Question_15/Diamond_shape.py
+++ Question_15/Diamond_shape.py
@@ -6,28 +6,3 @@ for i in range(loops-1):    #Bottom half of the diamond
     print(" "*(i+1) + "*"*(2*(loops-i-1)-1))    #print spaces and stars
 
 
-
-
-
-# Create Diamond Shape
-# import turtle
-
-# def draw_diamond(turtle):
-#     angle =31.6
-#     while angle < 250:
-#         turtle.forward(angle)
-#         turtle.right(angle)
-#         angle += 1
-
-# def main():
-#     window = turtle.Screen()
-#     window.bgcolor("white")
-#     brad = turtle.Turtle()
-#     brad.shape("turtle")
-#     brad.color("black")
-#     brad.speed(50)
-#     draw_diamond(brad)
-#     window.exitonclick()
-
-# if __name__ == '__main__':
-#     main()
